9b47e659d4eec3cd342e9b7311ffb64d2117008e
Fey Ijaware <fshola4real@yahoo.co.uk>
2017-07-09 14:12:53 +0100

    First Commit

---
+++ app/src/main/java/com/example/android/reportcard/ReportCard.java
@@ -0,0 +1,77 @@
+package com.example.android.reportcard;
+
+/**
+ * Created by FEY-AGAPE on 09/07/2017.
+ */
+
+public class ReportCard {
+    private String schoolName;
+    private String studentName;
+    private int gradeMathematics;
+    private int gradeEnglish;
+    private int gradeScience;
+
+    public ReportCard(String school, String name, int gradeM, int gradeE, int gradeS, int gradeL) {
+        schoolName = school;
+        studentName = name;
+        gradeMathematics = gradeM;
+        gradeEnglish = gradeE;
+        gradeScience = gradeS;
+    }
+
+    //Get the School name
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String school) {
+        schoolName = school;
+    }
+
+    //Get the Student name
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String name) {
+        studentName = name;
+    }
+
+    //For the Mathematics grade
+    public int getGradeMathematics() {
+        return gradeMathematics;
+    }
+
+    public void setGradeMathematics(int gradeM) {
+        gradeMathematics = gradeM;
+    }
+
+    //For the English grade
+    public int getGradeEnglish() {
+        return gradeEnglish;
+    }
+
+    public void setGradeEnglish(int gradeC) {
+        gradeEnglish = gradeC;
+    }
+
+    //For the Science grade
+    public int getGradeScience() {
+        return gradeScience;
+    }
+
+    public void setGradeScience(int gradeS) {
+        gradeScience = gradeS;
+    }
+
+
+    @Override
+    public String toString() {
+
+        return "School Name: " + schoolName + "\n" +
+                "Student Name: " + studentName + "\n" +
+                "Mathematics: " + gradeMathematics + "\n" +
+                "English: " + gradeEnglish+ "\n" +
+                "Science: " + gradeScience;
+    }
+}
